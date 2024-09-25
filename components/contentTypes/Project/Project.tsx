b52784108afc2bb1172779027b0aebb98c95848a
Bence Husi <bence@ollaladesign.nl>
2024-09-26 01:59:17 +0200

    Add colophon bottom padding

--- components/contentTypes/Project/Project.tsx
+++ components/contentTypes/Project/Project.tsx
@@ -3,16 +3,16 @@ import { storyblokEditable, StoryblokComponent } from "@storyblok/react/rsc";
 
 export function Project({ blok }: any) {
   return (
-    <main
+    <div
       {...storyblokEditable(blok)}
       className="mx-auto my-10 max-w-[680px] px-5"
     >
       <h1 className="mb-6 text-lg font-bold">{blok?.title}</h1>
-      <div className="space-y-11">
+      <div className="space-y-11 pb-6">
         {blok?.content?.map((nestedBlok: any) => (
           <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
         ))}
       </div>
-    </main>
+    </div>
   );
 }
