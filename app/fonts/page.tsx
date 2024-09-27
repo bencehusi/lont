c05f9eedcc9577a5faa3c0d3b45ca4a84c3b4965
Bence Husi <bence@ollaladesign.nl>
2024-09-28 01:16:02 +0200

    Metadata fixes

--- app/fonts/page.tsx
+++ app/fonts/page.tsx
@@ -2,6 +2,14 @@ import { fetchStories } from "@/lib/storyblok";
 import { FontItem } from "./components/FontItem";
 import { Story } from "@/@types/storyblok";
 
+export async function generateMetadata() {
+  return {
+    title: "Fonts | Library of Narrative Types",
+    description:
+      "Elevate your design work with distinctive fonts from the Library of Narrative Types. Unique typefaces that tell your story..",
+  };
+}
+
 export default async function Fonts() {
   const { stories: fonts } = await fetchStories({
     starts_with: "fonts",
--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,3 +1,31 @@
+import Link from "next/link";
+
+/* Metadata */
+export const metadata = {
+  title: "Shop | Library of Narrative Types",
+  description: "Coming soon. Be sure to check back later.",
+};
+
 export default function Fonts() {
-  return <div>Shop...</div>;
+  return (
+    <div className="h-full grow overflow-y-auto rounded-xl border-2 border-black px-4 py-10 sm:px-6 md:py-16">
+      <div className="mx-auto max-w-3xl">
+        <h1 className="mb-4 text-xl font-bold md:text-3xl">Shop</h1>
+        <p className="mb-8">Coming soon. Be sure to check back later.</p>
+        <p className="mb-3">We have other pages you can visit:</p>
+        <ul>
+          <li>
+            <Link className="underline" href="/fonts">
+              Fonts
+            </Link>
+          </li>
+          <li>
+            <Link className="underline" href="/">
+              Projects
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
