45f990740746c58513779fd44e1ba83608cd50c0
Bence Husi <bence@ollaladesign.nl>
2024-09-28 14:29:10 +0200

    Redo live cache

--- app/fonts/[slug]/page.tsx
+++ app/fonts/[slug]/page.tsx
@@ -44,7 +44,6 @@ export default async function FontPage({ params }: PageProps) {
   const { slug } = params;
   const font = await fetchStories({
     slug: [`fonts/${slug}`],
-    cv: Date.now(),
   });
   const products = await stripe.products.list({
     limit: 100,
